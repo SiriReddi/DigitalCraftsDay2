ffc9fedb438d1cce0d08a27fe983f1891fedbe3d
Gopi Gongalareddy <siribasireddi@gmail.com>
2019-08-30 15:45:31 -0400

    Python exerc from day2

---
+++ capsname.py
@@ -0,0 +1,3 @@
+name = input("What is your name : ")
+print("Hello, " + name.upper() + "!")
+print("Count of name: " + str(len(name)))
---
+++ numcalculations.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the Number: "))
+print(type(num))
+result = num * num
+print(result)
+result = num + num
+print(result)
+
